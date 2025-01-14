706ad3e89d7480550d2c5753f145a7ba27910913
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-14 11:20:18 -0500

    Change db driver to libsql

--- server/db/connection.go
+++ server/db/connection.go
@@ -5,12 +5,12 @@ import (
 	"os"
 
 	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/tursodatabase/go-libsql"
 )
 
 func NewConnection() (*sqlx.DB, error) {
 	dbUrl := os.Getenv("DATABASE_URL")
-	db, err := sqlx.Connect("sqlite3", dbUrl)
+	db, err := sqlx.Connect("libsql", dbUrl)
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
