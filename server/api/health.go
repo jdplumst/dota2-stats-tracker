152ac6a903de4663004a086c62f7c2637eaa29c9
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-14 08:59:37 -0500

    Add cors helper function

--- server/api/health.go
+++ server/api/health.go
@@ -3,14 +3,12 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+
+	utils "github.com/jdplumst/dota2-stats-tracker/server/utils"
 )
 
 func HealthHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Credentials", "true")
-	w.Header().
-		Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	utils.SetCors(w)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	resp := make(map[string]string)
---
+++ server/utils/cors.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"net/http"
+)
+
+func SetCors(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+}
