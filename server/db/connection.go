8e78b1ec0480aa4ff2c22aada8f78c18891370bb
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-14 11:31:59 -0500

    Fix build error

--- server/db/connection.go
+++ server/db/connection.go
@@ -2,16 +2,19 @@ package db
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jmoiron/sqlx"
-	_ "github.com/tursodatabase/go-libsql"
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func NewConnection() (*sqlx.DB, error) {
 	dbUrl := os.Getenv("DATABASE_URL")
 	db, err := sqlx.Connect("libsql", dbUrl)
 	if err != nil {
+		log.Println("Error connecting to database", err)
 		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 
